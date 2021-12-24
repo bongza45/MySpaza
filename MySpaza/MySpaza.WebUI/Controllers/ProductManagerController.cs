a8dd1f3d233eafe14e735776c66144d26301e2f6
bongza45 <royalbongza@gmail.com>
2021-12-24 20:21:13 +0200

    ProductImages done

--- MySpaza/MySpaza.WebUI/Controllers/ProductManagerController.cs
+++ MySpaza/MySpaza.WebUI/Controllers/ProductManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -34,7 +35,7 @@ namespace MySpaza.WebUI.Controllers
             return View(viewModel);
         }
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product,HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
@@ -42,6 +43,11 @@ namespace MySpaza.WebUI.Controllers
             }
             else
             {
+                if (file != null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image);
+                }
                 context.Insert(product);
                 context.Commit();
                 return RedirectToAction("Index");
@@ -63,7 +69,7 @@ namespace MySpaza.WebUI.Controllers
             }
         }
         [HttpPost]
-       public ActionResult Edit(Product product, string Id)
+       public ActionResult Edit(Product product, string Id, HttpPostedFileBase file)
         {
             Product productToEdit = context.Find(Id);
             if (productToEdit == null)
@@ -76,11 +82,15 @@ namespace MySpaza.WebUI.Controllers
                 {
                     return View(product);
                 }
+                if (file != null)
+                {
+                    productToEdit.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + productToEdit.Image);
+                }
                 productToEdit.Category = product.Category;
                 productToEdit.Description = product.Description;
                 productToEdit.Name = product.Name;
                 productToEdit.Price = product.Price;
-                productToEdit.Image = product.Image;
 
                 context.Commit();
                 return RedirectToAction("Index");
